5f684b96c149711b5d8bf39dfaa0899ebcc5fd37
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2024-11-16 11:01:43 +0000

    factorial committed

---
+++ PS-Sprint-1/Finding the Factorial of a Number.py
@@ -0,0 +1,13 @@
+# Write a program to compute the factorial of a given number.
+
+def factorial(n):
+    result = 1
+    if n < 0:
+        print("Invalid Input ")
+    else:
+        for i in range(1, n+1 ):
+            result = result * i 
+    print("Factorial of ",f'{n} is :', result)
+
+n = int(input("Enter a Number: "))
+factorial(n)
