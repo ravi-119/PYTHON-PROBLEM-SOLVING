095a61fa9e7297d1ebd9b9a42ac608bd1a866442
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2024-11-16 10:05:36 +0000

    Committed pyramid star

---
+++ PS-Sprint-1/Crafting Star Patterns.py
@@ -0,0 +1,29 @@
+# Write a program to create different star patterns (e.g., pyramid, diamond).
+
+def pyramid(n):
+    # The outer loop represents the no of rows 
+    for i in range(1, n+1):
+
+        # The first internal loop represents to print the no of spaces
+        for j in range(n-i):
+            print(" ", end="")
+
+        # The second internal loop represents to print the no of astrisks 
+        for k in range(1, 2*i):
+            print("*", end="")
+
+        print()
+
+# And Finathelly caling  functions 
+n = int(input("Enter no of height: "))
+pyramid(n)
+
+
+
+
+
+
+
+    
+
+
