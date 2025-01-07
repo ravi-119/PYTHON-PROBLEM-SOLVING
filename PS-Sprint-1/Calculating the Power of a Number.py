9f26ae70face092c7d3fd07e2788b14fd062dfad
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-07 17:00:20 +0000

    Calculating the Power of a Number committed

---
+++ PS-Sprint-1/Calculating the Power of a Number.py
@@ -0,0 +1,11 @@
+# write a program to Calculating the Power of a Number
+
+# Taking base value as a input from user
+base = int(input("Enter base value: "))
+# Taking exponent value as a input from user
+exponent = int(input("Enter exponent: "))
+# storing the result in a result variable
+result = base ** exponent
+# Printing the result
+print(f"{base} raised to the power of {exponent} is : ",result)
+
