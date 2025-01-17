bdae9ae9a6a5e54c71cae4565f813106d1c430c7
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-17 16:53:05 +0530

    Finding All Divisors of a Number

---
+++ PS-Sprint-1/Finding All Divisors of a Number.py
@@ -0,0 +1,14 @@
+# Finding All Divisors or factor of a Number
+# Taking a number from user as an input
+num = int(input(" Enter a number : "))
+# init a factor variable to store factors 
+factor = []
+
+# iterating from one to num 
+for i in range(1, num+1):
+    # checking condition 
+    if num % i == 0:
+        factor.append(i)
+# Printing the result 
+print(factor)
+
