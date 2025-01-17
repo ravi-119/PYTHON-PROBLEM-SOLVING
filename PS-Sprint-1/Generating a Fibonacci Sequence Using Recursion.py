96339f486301733543a7c8bf922f59b3550dcde3
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-17 17:49:04 +0530

    Update Generating a Fibonacci Sequence Using Recursion.py

--- PS-Sprint-1/Generating a Fibonacci Sequence Using Recursion.py
+++ PS-Sprint-1/Generating a Fibonacci Sequence Using Recursion.py
@@ -22,3 +22,21 @@ for i in range(num):
 
 
     
+"""
+    Example walkthrough (if num == 6 ):
+    iteartion 1 (i = 0 ): fibonacci(0) = 0 -> Print 0.
+    iteartion 2 (i = 1 ): fibonacci(1) = 1 -> Print 1.
+    iteartion 3 (i = 2 ): fibonacci(2) =  fibonacci(1) +  fibonacci(0) = 1 + 0 = 1 -> Print 1.
+    iteartion 4 (i = 3 ): fibonacci(3) =  fibonacci(2) +  fibonacci(1) = 1 + 1 = 2 -> Print 2.
+    iteartion 5 (i = 4 ): fibonacci(4) =  fibonacci(3) +  fibonacci(2) = 2 + 1 = 3 -> Print 3.
+    iteartion 6 (i = 5 ): fibonacci(5) =  fibonacci(4) +  fibonacci(3) = 3 + 2 = 5 -> Print 5.
+    
+"""
+
+
+
+
+
+
+
+
