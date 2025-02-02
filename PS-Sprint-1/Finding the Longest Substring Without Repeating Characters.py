9a90e63bae0ccd1c6b3e9bb34c7a42e8b794ae71
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-02-03 00:31:55 +0530

    Update Finding the Longest Substring Without Repeating Characters

--- PS-Sprint-1/Finding the Longest Substring Without Repeating Characters.py
+++ PS-Sprint-1/Finding the Longest Substring Without Repeating Characters.py
@@ -6,17 +6,23 @@ def length_of_longest_substring(s: str) -> int:
     left = 0
     max_length = 0
 
-    # Iterate Through the String
+    # Iterate Through the String left to right
     for right in range(len(s)):
         # Check for Duplicates
         while s[right] in char_set:
+            # Remove the leftmost character
             char_set.remove(s[left])
+            # Move left pointer to shrink the window
             left += 1
         # Expand the Window and Update Maximum Length
+        # Add new unique character to the set
         char_set.add(s[right])
+        # Update max_length
         max_length = max(max_length, right - left + 1)
     # Return the Result
     return max_length
+
+
 
 # Example Usage
 s = "abcabcbb"
