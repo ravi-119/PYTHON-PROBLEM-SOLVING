5b8848624ba05b453204a9a1d5001860ccad0f29
ravi-119 <ry277015@gmail.com>
2025-02-04 19:22:32 +0530

    addition more added

--- PS-Sprint-2/hello.py
+++ PS-Sprint-2/hello.py
@@ -2,3 +2,11 @@ num1 = int(input("Enter num1 : "))
 num2 = int(input("Enter num2 : "))
 result = num1 + num2 
 print("The Result is :", result)
+
+
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+result = num1 + num2 
+print("The Result is :", result)
+
