be8bc6682a49044aa5bcc86966d5a38f6990ed5a
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-02-12 00:54:25 +0530

    Update Calculating the Sum of a Series.py

--- PS-Sprint-2/Calculating the Sum of a Series.py
+++ PS-Sprint-2/Calculating the Sum of a Series.py
@@ -10,3 +10,15 @@ n = int(input("Enter a number: "))
 
 # Compute the harmonic sum and print the result
 print(f"Harmonic sum for n={n}: {harmonic_sum(n)}")  
+
+
+
+"""
+Function Execution (harmonic_sum(4))
+total = 0
+k = 1: total += 1 / 1 = 1.0
+k = 2: total += 1 / 2 = 1.0 + 0.5 = 1.5
+k = 3: total += 1 / 3 = 1.5 + 0.3333 = 1.8333
+k = 4: total += 1 / 4 = 1.8333 + 0.25 = 2.0833
+Final total = 2.0833
+"""
